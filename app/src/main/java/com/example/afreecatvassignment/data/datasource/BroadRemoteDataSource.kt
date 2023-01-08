093bc72176d9c1630e2c120f16570fcbff825b7b
TRASALBY <ghtjd8264@naver.com>
2023-01-08 17:32:56 +0900

    Issue #16 refactor: DataSource부터 Flow 전달

--- app/src/main/java/com/example/afreecatvassignment/data/datasource/BroadRemoteDataSource.kt
+++ app/src/main/java/com/example/afreecatvassignment/data/datasource/BroadRemoteDataSource.kt
@@ -1,7 +1,8 @@
 package com.example.afreecatvassignment.data.datasource
 
 import com.example.afreecatvassignment.data.model.BroadCategoryItem
+import kotlinx.coroutines.flow.Flow
 
 interface BroadRemoteDataSource {
-    suspend fun getCategoryList(): List<BroadCategoryItem>
+    fun getCategoryList(): Flow<List<BroadCategoryItem>>
 }
--- app/src/main/java/com/example/afreecatvassignment/data/datasource/BroadRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/afreecatvassignment/data/datasource/BroadRemoteDataSourceImpl.kt
@@ -2,13 +2,22 @@ package com.example.afreecatvassignment.data.datasource
 
 import com.example.afreecatvassignment.data.api.AfreecaApiService
 import com.example.afreecatvassignment.data.model.BroadCategoryItem
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class BroadRemoteDataSourceImpl @Inject constructor(
     private val afreecaApiService: AfreecaApiService
 ) : BroadRemoteDataSource {
-    override suspend fun getCategoryList(): List<BroadCategoryItem> {
-        val a = afreecaApiService.getCategoryList()
-        return a.broadCategory
+    override fun getCategoryList(): Flow<List<BroadCategoryItem>> {
+        return flow {
+            runCatching {
+                afreecaApiService.getCategoryList()
+            }.onSuccess {
+                emit(it.broadCategory)
+            }.onFailure { exception ->
+                throw exception
+            }
+        }
     }
 }
